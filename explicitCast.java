a567794ab930efb7f6549853d091283b18ca3da4
Karina Kallas <kallask@oregonstate.edu>
2024-01-13 15:37:04 -0800

    explicit cast example

---
+++ explicitCast.java
@@ -0,0 +1,8 @@
+class ExplicitCast {
+    public static void main(String[] args) {
+        double aDouble = 20;
+        int castInt = (int)aDouble;
+
+        System.out.println("I have explicitly cast a double as an int: " + castInt);
+    }
+}
